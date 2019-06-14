29897df3128f3c3027329a0ed9b6f286229aa588
Nathaniel Brian <nbmbestre91@gmail.com>
2019-06-14 14:57:53 +0800

    remove unused routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule }    from '@angular/common/http';
-import { Routes, RouterModule } from '@angular/router';
 
 import { PhotoDetailComponent } from './photo-detail/photo-detail.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-const routes: Routes = [
-  { path: '', redirectTo: 'photos', pathMatch: 'full'},
-  { path: 'photos', component: AppComponent },
-  { path: ':id', component: PhotoDetailComponent },
-];
 
 @NgModule({
   imports: [ 
